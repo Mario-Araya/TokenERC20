1850625d1f25cb0327a9e5a00e5d28e155d02cb5
Mario Araya <arayaroyo686@gmail.com>
2024-05-29 14:07:27 -0600

    CoraToken Contract

---
+++ CoraToken.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+
+contract MyToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
+    constructor(address initialOwner)
+        ERC20("MyToken", "MTK")
+        ERC20Permit("MyToken")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 100000000 * 10 ** decimals());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
